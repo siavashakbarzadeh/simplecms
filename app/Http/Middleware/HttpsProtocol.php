0ca87c1cca94204dc8a7ce1a932cd2d195e465b2
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-03-24 14:01:43 +0200

    1401

---
+++ app/Http/Middleware/HttpsProtocol.php
@@ -0,0 +1,16 @@
+<?php
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class HttpsProtocol {
+    public function handle(Request $request, Closure $next)
+    {
+        if (!$request->secure() && app()->environment('production')) {
+            return redirect()->secure($request->getRequestUri());
+        }
+
+        return $next($request);
+    }
+}
--- config/app.php
+++ config/app.php
@@ -54,7 +54,7 @@ return [
     |
     */
 
-    'url' => env('APP_URL', 'http://localhost'),
+    'url' => env('APP_URL', 'https://simplecms-hlixxcbawa-uc.a.run.app'),
 
     'asset_url' => env('ASSET_URL'),
 
