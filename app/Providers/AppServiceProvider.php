0a86e1b8efd794be907c80484b4f22492551b669
Amirezalla <37926833+amirezalla@users.noreply.github.com>
2024-04-08 21:54:50 +0200

    2154

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -24,9 +24,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
 
-//        if ($this->app->environment('production')) {
-//            URL::forceScheme('https');
-//        }
+       if ($this->app->environment('production')) {
+           URL::forceScheme('https');
+       }
 
         $iv = openssl_random_pseudo_bytes(openssl_cipher_iv_length('aes-256-cbc'));
 
